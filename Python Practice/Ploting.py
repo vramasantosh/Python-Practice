6ac5c80f7a4c87239b9de0b8aa8e6abe07b3c3d3
vramasantosh <118262325+vramasantosh@users.noreply.github.com>
2023-02-14 17:05:54 +0530

    Add files via upload

---
+++ Python Practice/Ploting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import seaborn as sns
+
+
+# In[ ]:
+
+
+stdt = pd.read_csv("2009_Data")
+
